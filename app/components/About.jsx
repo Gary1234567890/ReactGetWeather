112df540c255723e2cf95d47311af61b7ba33992
Gary <gkclark@gmail.com>
2017-04-09 08:29:19 +0200

    added info to about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,18 @@ var About = (props) => {
     return (
         <div>
             <h1 className="text-center">About Page</h1>
-            <p>Welcome to the about page</p>
+            <p>Gets the weather from api.openweathermap.org and displays it using React.</p>
+            <p>
+                Here are some of the tools used to create the app.
+            </p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a> - This was the javascript framework used.
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">Open Weather Map</a> - API searched to get weather.
+                </li>
+            </ul>
         </div>
             
         )
